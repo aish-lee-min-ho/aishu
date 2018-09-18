482a5ee917c0f7a8e088aed9b311112ea79a0c17
aishwarya <aishwaryanandh@gmail.com>
2018-09-18 13:11:40 +0530

    second project

---
+++ pg3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int n1,n2,sum;
+	printf("enter the numbers: \n");
+	scanf("%d %d", &n1,&n2);
+	sum=n1+n2;
+	printf("sum of integers:%d \n\n",sum);
+	return 0;
+}
+
